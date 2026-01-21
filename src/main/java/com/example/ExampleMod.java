5ca1b75970b006f46f8f5ff5d48391ab6ee566d8
abdurrahman447 <abdurrahmansa31@gmail.com>
2026-01-21 23:02:56 +0300

    Yy

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,17 +2,19 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.ActionResult;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 
 public class SungerKirmaMod implements ModInitializer {
     @Override
     public void onInitialize() {
         AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if (!world.isClient && world.getBlockState(pos).getBlock() == Blocks.SPONGE) {
-                world.breakBlock(pos, true);
+            if (!world.isClientSide && world.getBlockState(pos).is(Blocks.SPONGE)) {
+                world.destroyBlock(pos, true);
             }
-            return ActionResult.PASS;
+            return InteractionResult.PASS;
         });
     }
 }
