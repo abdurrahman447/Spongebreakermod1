f22eea4c850d5ec921fa983797fdb81680b38a82
abdurrahman447 <abdurrahmansa31@gmail.com>
2026-01-21 22:47:15 +0300

    Implement block breaking on sponge attack

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,18 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.ActionResult;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+public class SungerKirmaMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if (!world.isClient && world.getBlockState(pos).getBlock() == Blocks.SPONGE) {
+                world.breakBlock(pos, true);
+            }
+            return ActionResult.PASS;
+        });
+    }
 }
